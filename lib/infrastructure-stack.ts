011022ffe5079e61c8478f7382fa8e5677c50bc8
lewisian8787 <lewisian8787@gmail.com>
2026-05-19 22:34:53 -0400

    initial code to deploy a single VPC with one AZ and a public and private subnet

--- lib/infrastructure-stack.ts
+++ lib/infrastructure-stack.ts
@@ -1,16 +1,31 @@
 import * as cdk from 'aws-cdk-lib/core';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const vpc = new ec2.Vpc(this, 'Vpc', {
+      //at current I have left as one az, as we will likley be using EBS, which is scoped to AZ zone.
+      maxAzs: 1,
+      natGateways: 1,
+      subnetConfiguration: [
+        {
+          //public subnets
+          name: 'Public',
+          subnetType: ec2.SubnetType.PUBLIC,
+          // leaving as default
+          cidrMask: 24,
+        },
+        {
+          //private subnets
+          name: 'Private',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 24,
+        },
+      ],
+    });
   }
 }
